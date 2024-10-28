292ae0040b840143488cd8918e5d29d6f9fe884c
DanielChau128 <ccaedrc@ucl.ac.uk>
2024-10-28 10:47:24 +0000

    Adds unit test for a backwards time - one where the set end time is before the set start time

--- test_times.py
+++ test_times.py
@@ -6,15 +6,6 @@ def test_generic_case():
     expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
     assert compute_overlap_time(large, short) == expected
 
-###
-# create a test each in test_times.py for:
-#   two time ranges that do not overlap
-#   two time ranges that both contain several intervals each
-#   two time ranges that end exactly at the same time when the other starts
-# run pytest and see whether all tests are picked up by pytest and whether they pass.
-# fix any bugs in times.py the tests may have helped you find.
-# Add the new and modified files to the repository, commit them (with a meaningful comment that also includes Answers UCL-COMP0233-24-25/RSE-Classwork#12) and push it to your fork.
-
 def test_no_overlap():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
@@ -32,5 +23,11 @@ def test_multiple_intervals():
 def test_same_end_start():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 12:00:00", "2010-01-12 12:15:00")
+    expected = []
+    assert compute_overlap_time(large, short) == expected
+
+def test_backwards_time():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    short = time_range("2010-01-12 11:30:00", "2010-01-12 11:15:00")
     expected = []
     assert compute_overlap_time(large, short) == expected
